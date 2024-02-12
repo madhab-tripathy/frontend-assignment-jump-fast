4c4bf4badfef3a0ec46a572f6ceb58ceaff39566
Madhab Tripathy <madhabtripathy1349@gmail.com>
2024-02-12 23:29:34 +0530

    api-key added

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,7 +1,7 @@
 export const getCatData = async () =>{
     try {
         // fetch data from 3rd party api
-        const response = await fetch(`https://api.thecatapi.com/v1/images/search?has_breeds=1&limit=20&api_key=${process.env.REACT_APP_API_KEY}`);
+        const response = await fetch(`https://api.thecatapi.com/v1/images/search?has_breeds=1&limit=20&api_key=live_0C04Q5Tg1tw0WeFhWWiFZQQ8JuBjvU8yYc71tYbVdUPibXNmWDXzlE2QZhrDG7Cb`);
         return await response.json();
 
     } catch (error) {
